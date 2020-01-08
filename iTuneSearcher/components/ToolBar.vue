b80648d87463b2210820a7605a47852c0f1b13fe
zitherSide <takuyakotoyori@akane.waseda.jp>
2020-01-08 21:05:19 +0900

    Made ToolBar component, and modified default layout.

---
+++ iTuneSearcher/components/ToolBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-toolbar dark color="blue">
+        <v-app-bar-nav-icon/>
+        <v-spacer/>
+        <v-toolbar-title class="white--text">
+            <nuxt-link class="title" to="/">iTuneSearcher</nuxt-link>
+        </v-toolbar-title>
+        <v-spacer/>
+        <v-btn to="/" icon>
+            <v-icon>mdi-refresh</v-icon>
+        </v-btn>
+    </v-toolbar>
+    
+</template>
+
+<script>
+
+</script>

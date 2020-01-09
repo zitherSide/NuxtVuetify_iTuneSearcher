dea3dbf77542b84cdb35d084dd3e8358c627568b
zitherSide <takuyakotoyori@akane.waseda.jp>
2020-01-09 20:14:36 +0900

    created index page.

--- iTuneSearcher/pages/index.vue
+++ iTuneSearcher/pages/index.vue
@@ -1,7 +1,23 @@
 <template>
+    <div>
+        <v-layout align-center column>
+            <h1>Search iTunes</h1>
+            <br/>
 
+            <v-text-field v-model="search" placeholder="Enter Artist Name" autofocus/>
+            <v-card>
+                <v-card-title>{{search}}</v-card-title>
+            </v-card>
+        </v-layout>
+    </div>
 </template>
 
 <script>
-
+export default{
+    data(){
+        return{
+            search:""
+        }
+    }
+}
 </script>

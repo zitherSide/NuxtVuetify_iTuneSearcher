ff24925d0d695e06cc86143eada5773d6e9f8254
zitherSide <takuyakotoyori@akane.waseda.jp>
2020-01-13 09:51:49 +0900

    implemented iTune search function.

--- iTuneSearcher/pages/results/_id.vue
+++ iTuneSearcher/pages/results/_id.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
         {{$route.params.id}}
+        {{albumData}}
     </div>
 </template>
 
 <script>
-export default{
+import axios from 'axios'
 
+export default{
+    asyncData({params}){
+        return axios.get(`https://itunes.apple.com/search?term=${params.id}&entity=album`)
+                .then( (response) =>{
+                    return {albumData: response.data.results}
+                })
+    }
 }
 </script>
